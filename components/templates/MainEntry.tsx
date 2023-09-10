52c1b8876397654f7bbbab8298e14b04e7d7fb5a
adarschwarzbach <adar.schwarzbach@gmail.com>
2023-09-09 23:58:33 -0400

    add loading screen

--- components/templates/MainEntry.tsx
+++ components/templates/MainEntry.tsx
@@ -4,18 +4,24 @@ import "@blueprintjs/core/lib/css/blueprint.css";
 import { useGlobalState } from "../../contexts/GlobalState";
 import Home from "../home/Home";
 import Report from "../report/Report";
+import LoadingScreen from "../home/Loading";
 
 export default function MainEntry() {
 	const state = useGlobalState();
 	const { darkMode, loading, report } = state;
 
+	if (loading) {
+		return <LoadingScreen />;
+	}
+  
 	return (
+    
 		<div
 			className={darkMode ? "bp5-dark" : ""}
 			style={{ width: "100vw", maxHeight: "100vh", overflow: "hidden" }}
 		>
 			<Header />{
-				!report ?
+				report ?
 					<Report />
 					:
 					<Home />}
